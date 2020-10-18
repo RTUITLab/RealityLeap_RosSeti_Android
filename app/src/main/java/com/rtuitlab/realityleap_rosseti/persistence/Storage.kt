d326492890b9602b5b25f1cc5a0590613bce9823
Nikita <almostroll@yandex.ru>
2020-10-18 03:43:26 +0300

    Add ability to make saves

--- app/src/main/java/com/rtuitlab/realityleap_rosseti/persistence/Storage.kt
+++ app/src/main/java/com/rtuitlab/realityleap_rosseti/persistence/Storage.kt
@@ -1,16 +1,20 @@
 package com.rtuitlab.realityleap_rosseti.persistence
 
 import android.content.Context
+import com.google.gson.Gson
 import com.rtuitlab.realityleap_rosseti.server.models.Employee
+import com.rtuitlab.realityleap_rosseti.server.models.InspectionResult
+import com.rtuitlab.realityleap_rosseti.server.models.InspectionTask
+import java.io.Serializable
 
-class Storage(
-    context: Context
-) {
+class Storage(context: Context) {
 
     var currentUser = Employee()
 
     private companion object {
         const val PREFERENCES_KEY = "PREFERENCES_KEY"
+        const val CURRENT_TASK_KEY = "CURRENT_TASK_KEY"
+        const val CURRENT_RESULT_KEY = "CURRENT_RESULT_KEY"
     }
 
     private val prefs = context.getSharedPreferences(
@@ -18,4 +22,24 @@ class Storage(
         Context.MODE_PRIVATE
     )
 
+    fun getSave(): Serializable? {
+        return prefs.getString(CURRENT_TASK_KEY, null)?.let {
+            Gson().fromJson(it, InspectionTask::class.java)
+        } ?: prefs.getString(CURRENT_RESULT_KEY, null)?.let {
+            Gson().fromJson(it, InspectionResult::class.java)
+        }
+    }
+
+    fun storeTask(task: InspectionTask) {
+        prefs.edit().putString(CURRENT_TASK_KEY, Gson().toJson(task)).apply()
+    }
+
+    fun storeResult(result: InspectionResult) {
+        deleteCurrentTask()
+        prefs.edit().putString(CURRENT_RESULT_KEY, Gson().toJson(result)).apply()
+    }
+
+    fun deleteCurrentTask() = prefs.edit().remove(CURRENT_TASK_KEY).apply()
+
+    fun deleteCurrentResult() = prefs.edit().remove(CURRENT_RESULT_KEY).apply()
 }
